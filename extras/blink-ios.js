4b383b9ce3476502c036d5465ae37951c5d18589
Alex Blackie <alex@blackie.me>
2022-09-30 23:25:04 -0400

    Add theme for Blink (iOS app)

---
+++ extras/blink-ios.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#131316", "#7B2000", "#617434", "#B66D3A", "#537397", "#5F4D84", "#508894", "#F1F1F3", "#595964", "#617434", "#26262B", "#B66D3A", "#537397", "#8E629D", "#508894", "#FAF9FA"]);
+t.prefs_.set('foreground-color', "#595964");
+t.prefs_.set('background-color', "#FAF9FA");
+t.prefs_.set('cursor-color', "#8E629D");
